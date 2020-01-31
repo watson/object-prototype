186e98e137315ac031eea1cae28c2c372063820b
Thomas Watson <w@tson.dk>
2020-01-31 22:59:40 +0100

    fix: ensure functions are not enumerable

--- index.js
+++ index.js
@@ -8,9 +8,15 @@ exports.ObjectPrototype = ObjectPrototype
 exports.create = () => Object.create(ObjectPrototype)
 exports.assign = (...args) => Object.assign(exports.create(), ...args)
 
+const descriptors = {}
+
 for (const name of functions) {
-  ObjectPrototype[name] = prototypelessFunction(Object.prototype[name])
+  const descriptor = Object.getOwnPropertyDescriptor(Object.prototype, name)
+  descriptor.value = prototypelessFunction(descriptor.value)
+  descriptors[name] = descriptor
 }
+
+Object.defineProperties(ObjectPrototype, descriptors)
 
 function prototypelessFunction (original) {
   const wrapper = function () {
